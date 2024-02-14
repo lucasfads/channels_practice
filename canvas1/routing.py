9c6434f6b7706e4dcb85ff25f7536c5620d2475a
Lucas Fads <lucasfads@gmail.com>
2024-02-14 15:35:00 -0300

    fix routing

--- canvas1/routing.py
+++ canvas1/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/canvas1/(?P<room_name>\w+)/$", consumers.Canvas1Consumer.as_asgi()),
+    re_path(r"ws/canvas1/", consumers.Canvas1Consumer.as_asgi()),
 ]
